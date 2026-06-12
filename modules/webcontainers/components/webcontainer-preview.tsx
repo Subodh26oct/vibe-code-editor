242a2438d41129191dd438c44b7b9918df6b7619
Subodh26oct <subodh261003kumar@gmail.com>
2026-06-12 17:14:21 +0530

    Fix: Add credentialless header and iframe attribute for WebContainer preview on Vercel 2.0

--- modules/webcontainers/components/webcontainer-preview.tsx
+++ modules/webcontainers/components/webcontainer-preview.tsx
@@ -494,6 +494,8 @@ const WebContainerPreview = ({
               src={previewUrl}
               className="w-full h-full border-none"
               title="WebContainer Preview"
+              // @ts-ignore - Enable loading iframe contents under Cross-Origin Isolation (COEP)
+              credentialless=""
             />
           </div>
 
